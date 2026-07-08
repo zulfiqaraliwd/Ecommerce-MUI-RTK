faeff1e77f3206a62f7c669258db853b000691e7
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-07-08 15:28:39 +0500

    make the app responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,12 +9,17 @@ import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
 import InputBase from "@mui/material/InputBase";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
 import { styled, alpha } from "@mui/material/styles";
 
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const navstyle = ({ isActive }) => ({
   color: isActive ? "#FFD700" : "white",
@@ -29,7 +34,6 @@ const Search = styled("div")(({ theme }) => ({
   "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  marginLeft: theme.spacing(2),
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
@@ -48,100 +52,215 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   "& .MuiInputBase-input": {
     padding: theme.spacing(1),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    width: "220px",
+    width: "100%",
   },
 }));
 
 function Navbar() {
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const totalQty = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
+  const totalQty = cartItems.reduce(
+    (sum, item) => sum + item.quantity,
+    0
+  );
+
+  const [open, setOpen] = React.useState(false);
 
   return (
-    <AppBar
-  position="static"
-  sx={{
-    bgcolor: "#1B3A2F", 
-  }}>
-      <Toolbar>
-        {/* Logo */}
-        <Box
-          component="img"
-          src="/assets/images/dryF-logo.png"
-          alt="logo"
-          sx={{
-            width: 70,
-            borderRadius: 10,
-          }}
-        />
+    <>
+      <AppBar
+        position="static"
+        sx={{
+          bgcolor: "#1B3A2F",
+        }}
+      >
+        <Toolbar>
 
-        <Typography
-          variant="h6"
-          sx={{
-            ml: 2,
-            fontWeight: "bold",
-          }}
-        >
-          Dry Fruits
-        </Typography>
+          {/* Mobile Menu */}
+          <IconButton
+            color="inherit"
+            sx={{
+              display: { xs: "block", md: "none" },
+              mr: 1,
+            }}
+            onClick={() => setOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
 
-      
-        {/* Navigation */}
-<Box
-  sx={{
-    display: "flex",
-    gap: 3,
-    ml: 20,
-  }}
->
-  <NavLink style={navstyle} to="/">
-    <Typography variant="subtitle1" fontWeight={500}>
-      Home
-    </Typography>
-  </NavLink>
+          {/* Logo */}
+          <Box
+            component="img"
+            src="/assets/images/dryF-logo.png"
+            alt="logo"
+            sx={{
+              width: { xs: 50, md: 70 },
+              borderRadius: 2,
+            }}
+          />
 
-  <NavLink style={navstyle} to="/about">
-    <Typography variant="subtitle1" fontWeight={500}>
-      About
-    </Typography>
-  </NavLink>
+          {/* Title */}
+          <Typography
+            variant="h6"
+            sx={{
+              ml: 1,
+              fontWeight: "bold",
+              fontSize: {
+                xs: "18px",
+                md: "24px",
+              },
+            }}
+          >
+            Dry Fruits
+          </Typography>
 
-  <NavLink style={navstyle} to="/contact">
-    <Typography variant="subtitle1" fontWeight={500}>
-      Contact
-    </Typography>
-  </NavLink>
+          {/* Desktop Navigation */}
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                md: "flex",
+              },
+              gap: 3,
+              ml: 6,
+            }}
+          >
+            <NavLink style={navstyle} to="/">
+              <Typography>Home</Typography>
+            </NavLink>
 
-  <NavLink style={navstyle} to="/newproducts">
-    <Typography variant="subtitle1" fontWeight={500}>
-      New Products
-    </Typography>
-  </NavLink>
-</Box>
+            <NavLink style={navstyle} to="/about">
+              <Typography>About</Typography>
+            </NavLink>
 
-        {/* Push Right */}
-        <Box sx={{ flexGrow: 1 }} />
+            <NavLink style={navstyle} to="/contact">
+              <Typography>Contact</Typography>
+            </NavLink>
 
-        {/* Search */}
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
+            <NavLink style={navstyle} to="/newproducts">
+              <Typography>New Products</Typography>
+            </NavLink>
+          </Box>
 
-          <StyledInputBase placeholder="Search Products..." />
-        </Search>
+          <Box sx={{ flexGrow: 1 }} />
 
-        {/* Cart */}<NavLink
-  to="/cart"
-  style={{ color: "inherit", textDecoration: "none" }}
->
-  <IconButton color="inherit" sx={{ ml: 2 }}>
-    <Badge badgeContent={totalQty} color="error">
-      <ShoppingCartIcon />
-    </Badge>
-  </IconButton>
-</NavLink>
-      </Toolbar>
-    </AppBar>
+          {/* Search */}
+          <Search
+            sx={{
+              width: {
+                xs: "120px",
+                sm: "180px",
+                md: "250px",
+              },
+            }}
+          >
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+
+            <StyledInputBase
+              placeholder="Search..."
+            />
+          </Search>
+
+          {/* Cart */}
+          <NavLink
+            to="/cart"
+            style={{
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <IconButton color="inherit">
+              <Badge
+                badgeContent={totalQty}
+                color="error"
+              >
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+
+      {/* Drawer */}
+
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Box sx={{ width: 250 }}>
+          <List>
+
+            <ListItem disablePadding>
+              <NavLink
+                to="/"
+                style={{
+                  width: "100%",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+                onClick={() => setOpen(false)}
+              >
+                <ListItemButton>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </NavLink>
+            </ListItem>
+
+            <ListItem disablePadding>
+              <NavLink
+                to="/about"
+                style={{
+                  width: "100%",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+                onClick={() => setOpen(false)}
+              >
+                <ListItemButton>
+                  <ListItemText primary="About" />
+                </ListItemButton>
+              </NavLink>
+            </ListItem>
+
+            <ListItem disablePadding>
+              <NavLink
+                to="/contact"
+                style={{
+                  width: "100%",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+                onClick={() => setOpen(false)}
+              >
+                <ListItemButton>
+                  <ListItemText primary="Contact" />
+                </ListItemButton>
+              </NavLink>
+            </ListItem>
+
+            <ListItem disablePadding>
+              <NavLink
+                to="/newproducts"
+                style={{
+                  width: "100%",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+                onClick={() => setOpen(false)}
+              >
+                <ListItemButton>
+                  <ListItemText primary="New Products" />
+                </ListItemButton>
+              </NavLink>
+            </ListItem>
+
+          </List>
+        </Box>
+      </Drawer>
+    </>
   );
 }
 
